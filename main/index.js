a5554036883d8ad48d59fd248003e98068a60b24
VictorR29 <victormramonesd@gmail.com>
2022-10-20 18:52:05 -0500

    Programando validación de selección de puntuación

--- main/index.js
+++ main/index.js
@@ -6,9 +6,11 @@ const ratingBtn = document.getElementById("btnSubmit")
 rating.addEventListener("click", (e)=>{
   let numberRating = e.target.value;
   ratingSelect.innerText = numberRating;
-});
 
-ratingBtn.addEventListener("click", ()=>{
-  document.querySelector(".ratingState").style.display = "none";
-  document.querySelector(".thanksState").style.display = "flex"; 
+  if(numberRating <= 5) {
+    ratingBtn.addEventListener("click", ()=>{
+      document.querySelector(".ratingState").style.display = "none";
+      document.querySelector(".thanksState").style.display = "flex"; 
+    });
+  };
 });
