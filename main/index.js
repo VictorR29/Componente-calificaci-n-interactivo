7c16d8011ccb591db391f86a3a6a6068af686149
VictorR29 <victormramonesd@gmail.com>
2022-10-20 18:44:22 -0500

    Implementando index.js y ajustando archivos html y css

--- main/index.js
+++ main/index.js
@@ -1,1 +1,14 @@
-console.log("Hola que tal")
+
+const rating = document.querySelector(".rating");
+const ratingSelect = document.querySelector(".ratingPoint");
+const ratingBtn = document.getElementById("btnSubmit")
+
+rating.addEventListener("click", (e)=>{
+  let numberRating = e.target.value;
+  ratingSelect.innerText = numberRating;
+});
+
+ratingBtn.addEventListener("click", ()=>{
+  document.querySelector(".ratingState").style.display = "none";
+  document.querySelector(".thanksState").style.display = "flex"; 
+});
